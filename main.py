8a397240e17168e42411114a83aab76eaa3362a0
Ema <116657478+emacrne@users.noreply.github.com>
2024-05-22 01:45:48 +0200

    added time

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 import os
 from feedback_arc_set import feedback_arc_set
+import time
 
 from networkit_fas import NetworkitGraph
 
 if __name__ == "__main__":
 
-    """if len(sys.argv) < 2:
+    if len(sys.argv) < 2:
         print("Usage: python script.py <folder_path>")
         sys.exit(1)
 
@@ -20,14 +21,19 @@ if __name__ == "__main__":
         if filename.endswith(".al"):
             file_path = os.path.join(folder_path, filename)
             graph = NetworkitGraph.load_from_adjacency_list(file_path)
+            start_time = time.time()
             arcset = feedback_arc_set(graph, use_smartAE=True, reduce=True)
-            print(filename, ": ", len(arcset))"""
+            end_time = time.time()
+            total_time = end_time - start_time
+            
+            print(f"{filename:<40}{len(arcset):<15}{total_time:.4f}")
 
-    if len(sys.argv) < 2:
+    # TODO: primer k crkne
+    """if len(sys.argv) < 2:
         sys.exit(f"Usage: python {sys.argv[0]} [filename]")
     file_path = os.path.join("random-large", "randomlarge-0003-25-30.al")
     graph = NetworkitGraph.load_from_adjacency_list(file_path)
     arcset = feedback_arc_set(graph, use_smartAE=True, reduce=True)
 
     print('result arc set:')
-    print(arcset)
+    print(arcset)"""
